c8fa1212f1021a546741357b339ea77a18ed1323
StevenLie14 <149150124+StevenLie14@users.noreply.github.com>
2025-05-19 01:19:07 +0700

    feat : add docker file for backend

--- backend/src/main/java/com/zaraclone/backend/BackendApplication.java
+++ backend/src/main/java/com/zaraclone/backend/BackendApplication.java
@@ -1,20 +1,18 @@
 package com.zaraclone.backend;
 
-import com.zaraclone.backend.repositories.CategoryRepository;
-import com.zaraclone.backend.repositories.ProductRepository;
-import com.zaraclone.backend.repositories.UserRepository;
-import lombok.AllArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableJpaRepositories
 public abstract class BackendApplication{
 
     public static void main(String[] args) {
+
+
         SpringApplication.run(BackendApplication.class, args);
     }
 
--- backend/src/test/java/com/zaraclone/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/zaraclone/backend/BackendApplicationTests.java
@@ -3,7 +3,7 @@ package com.zaraclone.backend;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = com.zaraclone.backend.BackendApplication.class)
 class BackendApplicationTests {
 
     @Test
